62069fb9070c8bb21e130ef12e8579a9a576705b
BhanuprasadDR <bhanuprasadmani99@gmail.com>
2025-11-25 11:20:39 +0530

    Built JARs and added Dockerfiles for deployment

---
+++ config-service/src/test/java/com/example/st/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
